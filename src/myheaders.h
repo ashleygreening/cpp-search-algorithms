1f16375be78986c3a5a3742f4b060404912cd3a2
ashleygreening <agreen31@villanova.edu>
2025-11-17 22:25:03 -0500

    updated selectionSort

--- src/myheaders.h
+++ src/myheaders.h
@@ -17,7 +17,7 @@ void	mergesort(long data[], long first, long last);
 void	merge(long d[], long first, long last);// sortB
 void    InsertionSort(long data[],long size);
 void    Insert(long data[],long size,long start,long stop);// sortC
-void    selectionsort(T data[], int n);
+void    selectionSort(T data[], int n); //sortB
 
 
 
